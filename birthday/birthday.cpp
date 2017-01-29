5e4af312739a448bc5736ff62371ab7bae8b0434
azlaan4 <azlaanzubair4@gmail.com>
2017-01-29 20:43:22 +0500

    a program to wish birthday to a friend

---
+++ birthday/birthday.cpp
@@ -0,0 +1,59 @@
+#include<windows.h>
+#include<iostream>
+#include<stdlib.h> //for system("cls");
+using namespace std;
+
+int main()
+{
+ int i;
+ char h=3;
+ system("cls");
+ system("color 1B"); //f->white background, 5->purple text
+ cout<<"\t\t\tFrom: Muhammad Azlaan Zubair\n";
+ Sleep(900);
+ cout<<"\t\t\tTo  : Syed Muhammad Danial\n\n";
+ Sleep(900);
+ cout<<"\t\t";
+ for(i=0;i<42;i++)
+ cout<<"_";
+ cout<<"\n\n";
+ system("color 1A"); //f->white background, 9->light blue text
+
+ Sleep(900);
+ cout<<"\t\t\t\ti i i i i\n";
+
+ Sleep(900);
+ cout<<"\t\t\t\ti i i i i\n";
+
+ Sleep(900);
+ cout<<"\t\t\t      __i_i_i_i_i__\n";
+
+ Sleep(900);
+ cout<<"\t\t\t     |"<<h<<"           "<<h<<"|\n";
+
+ Sleep(900);
+ cout<<"\t\t\t   __| "<<h<<"  29/01  "<<h<<" |__\n";
+
+ Sleep(900);
+ cout<<"\t\t\t  | "<<h<<"   "<<h<<"       "<<h<<"   "<<h<<" |\n";
+
+ Sleep(900);
+ cout<<"\t\t\t__|"<<h<<"   HAPPY     "<<h<<"   "<<h<<"|__\n";
+
+ Sleep(900);
+ cout<<"\t\t       | "<<h<<"   "<<h<<"     BIRTHDAY    "<<h<<" |\n";
+
+ Sleep(900);
+ cout<<"\t\t       |"<<h<<"  "<<h<<"     "<<h<<"    "<<h<<"     "<<h<<"   "<<h<<"|\n";
+
+
+ Sleep(900);
+ cout<<"\t\t       |"<<h<<" "<<h<<" "<<h<<"  D A N I A L  "<<h<<" "<<h<<" "<<h<<"|\n";
+
+ Sleep(900);
+ cout<<"\t\t       |_________________________|\n";
+
+ cout<<"\n\n\n\n\n";
+ system("pause");
+ return 0;
+}
