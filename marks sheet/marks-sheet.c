2bd71f5c1e8c580ce9fa7ca7aac985a897a928eb
azlaan4 <azlaanzubair4@gmail.com>
2017-01-30 21:45:00 +0500

    marksheet program

---
+++ marks sheet/marks-sheet.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+void mail(){
+
+// declaration of arrays
+
+    char exa[500];
+    char rol[100];
+    char name[500];
+    char fname[500];
+    char sch[1000];
+
+// declaration of variable
+
+    float sin, urd, isl, pst, biop, biot,chp, cht, phyp, phyt, engI, engII, math, compp, compt;
+    float total, pcent, obt;
+
+//  taking input from user
+
+    printf("Enter the Year of Examination: ");
+    scanf("%s", exa);
+    printf("Enter the Roll Number: ");
+    scanf("%s", rol);
+    printf("Enter your Name: ");
+    scanf("%s", name);
+    printf("Enter your Father's Name: ");
+    scanf("%s", fname);
+    printf("Enter your School Name: ");
+    scanf("%s", sch);
+    printf("Enter your marks in Sindhi: ");
+    scanf("%f", &sin);
+    printf("Enter your marks in Urdu: ");
+    scanf("%f",&urd);
+    printf("Enter your marks in Islamiat: ");
+    scanf("%f",&isl);
+    printf("Enter your marks in Pakistan Studies: ");
+    scanf("%f",&pst);
+    printf("Enter your marks in Biology Practical: ");
+    scanf("%f",&biop);
+    printf("Enter your marks in Biology Theory: ");
+    scanf("%f",&biot);
+    printf("Enter your marks in Chemistry Practical: ");
+    scanf("%f",&chp);
+    printf("Enter your marks in Chemistry Theory: ");
+    scanf("%f",&cht);
+    printf("Enter your marks in Physics Practical: ");
+    scanf("%f",&phyp);
+    printf("Enter you marks in Physics Theory: ");
+    scanf("%f",&phyt);
+    printf("Enter your marks in Computer Practical: ");
+    scanf("%f",&compp);
+    printf("Enter you marks in Computer Theory: ");
+    scanf("%f",&compt);
+    printf("Enter your marks in English Part I: ");
+    scanf("%f",&engI);
+    printf("Enter your marks in English Part II: ");
+    scanf("%f",&engII);
+    printf("Enter your marks in Mathematics: ");
+    scanf("%f",&math);
+
+// started calculating the input
+
+//    obt = sin+urd+isl+pst+biop+biot+chp+cht+phyp+phyt+engI+engII+math+compp+compt;
+//    printf("%f", obt);
+
+
+
+
+
+    getch();
+}
+
