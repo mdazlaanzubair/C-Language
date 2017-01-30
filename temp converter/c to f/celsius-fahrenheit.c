b610434427b786de0f788ab8d54af573cb3f6c9f
azlaan4 <azlaanzubair4@gmail.com>
2017-01-30 18:49:08 +0500

    celsius to farhrenheit converter

---
+++ temp converter/c to f/celsius-fahrenheit.c
@@ -0,0 +1,15 @@
+//C program to convert Fahrenheit to Celsius
+#include <stdio.h>
+
+int main()
+{
+    float celsius, fahrenheit;
+
+    printf("\nEnter temp in Celsius : ");
+    scanf("%f", &celsius);
+
+    fahrenheit = (1.8 * celsius) + 32;
+    printf("\nTemperature in Fahrenheit : %f ", fahrenheit);
+
+    return (0);
+}
