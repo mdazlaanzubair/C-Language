eda412cc5f361f1b4fd4a3dc41bb0325f4f284b1
azlaan4 <azlaanzubair4@gmail.com>
2017-02-09 23:39:57 +0500

    5 number skipping task

---
+++ number skipping/skiper.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main() {
+   int i = 5;
+
+   printf("\n");
+
+   printf(" Printing + 5 series: \n\n");
+
+   for (i = 45; i <= 65; i=i+5) {
+      printf("\t%d\t", i);
+   }
+
+   printf("\n");
+
+   printf(" Printing - 5 series: \n\n");
+
+   for (i = 65; i >= 45; i=i-5) {
+      printf("\t%d\t", i);
+   }
+
+   printf("\n");
+
+   return (0);
+}
