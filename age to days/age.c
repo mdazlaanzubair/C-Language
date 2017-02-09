ad93e3f8fe0c47de17305e9ea7cfa1b9d124b25d
azlaan4 <azlaanzubair4@gmail.com>
2017-02-09 19:55:28 +0500

    commiting age converter

--- age to days/age.c
+++ age to days/age.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+int main(){
 
+    int age, days;
+    system("cls");
+    days = 365;
+    printf("Please Enter Your Correct Age: ");
+    scanf("%d",&age);
+    days = age*365;
+    printf("\n\n\nYou age in days: %d\n\n", days);
+    return 0;
+
+}
