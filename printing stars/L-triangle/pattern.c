211036e5055a39e13291588c6e1dcc576b55bbe4
azlaan4 <azlaanzubair4@gmail.com>
2017-02-17 14:52:16 +0500

    L-Triangle 90 degree

---
+++ printing stars/L-triangle/pattern.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+int i,j,k;
+for (i=1;i<=10;i++){
+//    for(k=1;k<=10;k++){
+//        printf(" ");
+//    }
+    for(j=1;j<=i;j++){
+        printf("*");
+    }
+    printf("\n");
+}
+getch();
+}
