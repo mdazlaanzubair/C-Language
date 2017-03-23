533b6221e8de5f473226935520f46f392ec44676
Azlaan4 <azlaanzubair4@gmail.com>
2017-03-23 15:02:34 +0500

    lab task 2

--- calculator/cal.c
+++ calculator/cal.c
@@ -1,24 +1,26 @@
-#include <stdio.h>
-int main(){
-int num1,num2,num3,num4,num5,sum,avg,sqr,cube;
-printf("Enter Your First Number: ");
-scanf("%d",&num1);
-printf("Enter Your Second Number: ");
-scanf("%d",&num2);
-printf("Enter Your Third Number: ");
-scanf("%d",&num3);
-printf("Enter Your Fourth Number: ");
-scanf("%d",&num4);
-printf("Enter Your Fifth Number: ");
-scanf("%d",&num5);
-sum = num1+num2+num3+num4+num5;
-sqr = sum*sum;
-cube = sum*sum*sum;
-avg = sum/5;
-system("cls");
-printf("\n\tSquare of Your Numbers is %d,\n\n", sqr);
-printf("\tCube of Your Numbers is %d,\n\n", cube);
-printf("\tAverage of Your Number is %d.\n\n", avg);
+#include<stdio.h>
+#include<conio.h>
+void main (void)
+{
+ char ch;
+ float num1,num2;
+ clrscr();
+ printf("What do you want to do?\n");
+ printf("Addition, Subtraction, Multiplication or Division:");
+ ch=getche();
+ printf("\nEnter first number:");
+ scanf("%f",&num1);
+ printf("\nEnter second number:");
+ scanf("%f",&num2);
+ printf("\n\n");
+
+if(ch=='A')
+ printf("%f %c %f = %.2f",num1,"+",num2,num1+num2);
+if(ch=='S')
+ printf("%f %c %f = %.2f",num1,"-",num2,num1-num2);
+if(ch=='M')
+ printf("%f %c %f = %.2f",num1,"*",num2,num1*num2);
+if(ch=='D')
+ printf("%f %c %f = %.2f",num1,"/",num2,num1/num2);
 getch();
 }
-
