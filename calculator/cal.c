28176cd10615791296c8ec34a333d7dea9c40a64
Azlaan4 <azlaanzubair4@gmail.com>
2017-03-23 15:09:13 +0500

    modification in view

--- calculator/cal.c
+++ calculator/cal.c
@@ -4,9 +4,10 @@ void main (void)
 {
  char ch;
  float num1,num2;
- clrscr();
- printf("What do you want to do?\n");
- printf("Addition, Subtraction, Multiplication or Division:");
+// clrscr();
+ system("cls");
+ printf("What do you want to do?\n\n");
+ printf("Addition, Subtraction, Multiplication or Division:\n");
  ch=getche();
  printf("\nEnter first number:");
  scanf("%f",&num1);
@@ -14,13 +15,13 @@ void main (void)
  scanf("%f",&num2);
  printf("\n\n");
 
-if(ch=='A')
- printf("%f %c %f = %.2f",num1,"+",num2,num1+num2);
-if(ch=='S')
- printf("%f %c %f = %.2f",num1,"-",num2,num1-num2);
-if(ch=='M')
- printf("%f %c %f = %.2f",num1,"*",num2,num1*num2);
-if(ch=='D')
- printf("%f %c %f = %.2f",num1,"/",num2,num1/num2);
+if(ch=='A' || ch=='a')
+ printf("%.2f + %.2f = %.2f",num1,num2,num1+num2);
+if(ch=='S'|| ch=='s')
+ printf("%.2f - %.2f = %.2f",num1,num2,num1-num2);
+if(ch=='M'|| ch=='m')
+ printf("%.2f x %.2f = %.2f",num1,num2,num1*num2);
+if(ch=='D'|| ch=='d')
+ printf("%.2f / %.2f = %.2f",num1,num2,num1/num2);
 getch();
 }
