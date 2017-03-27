394db7e1375f91c8dce532b40e4ea31ec7982089
Azlaan4 <azlaanzubair4@gmail.com>
2017-03-27 23:10:15 +0500

    roos information completed

--- Project (Fundamental Programing)/project.c
+++ Project (Fundamental Programing)/project.c
@@ -3,27 +3,29 @@
 #include<stdlib.h>
 int main(){
 
-system("Color 79");
-system("cls");
+// backgroung and text color
 
-// decription of hotel
+    system("Color 79");
 
-printf("\n\n\n\n");
-printf("\t\t\t\t\t\t ***************************\n");
-printf("\t\t\t\t\t\t * Hotel Management System *\n");
-printf("\t\t\t\t\t\t ***************************");
-printf("\n\n\n\n");
-printf("\t\t\t\tHotel YAQ is one of the most newest Hotel in Karachi. The Hotel\n");
-printf("\t\t\tis equipped with all the general amenities and facilities that go\n");
-printf("\t\t\talong with memorable stay. Set amidst beautifully landscaped gardens,\n");
-printf("\t\t\tit proves to be a ideal dream destination for preceptive traveler.\n");
-printf("\t\t\tthe Hotel have well furnished rooms along with rooms providing pleasant\n");
-printf("\t\t\tviews of the city. The hotel satisfies the needs of business as well\n");
-printf("\t\t\tas the leisure traveler. All the rooms at the Hotel are furnished\n");
-printf("\t\t\tbeautifully. All the rooms are fitted with amenities.\n");
-printf("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t Press any key to continue");
-getch();
-system("cls");
+// description of hotel
+
+    system("cls");
+        printf("\n\n\n\n");
+        printf("\t\t\t\t\t\t ***************************\n");
+        printf("\t\t\t\t\t\t * Hotel Management System *\n");
+        printf("\t\t\t\t\t\t ***************************");
+        printf("\n\n\n\n");
+        printf("\t\t\t\tHotel YAQ is one of the most newest Hotel in Karachi. The Hotel\n");
+        printf("\t\t\tis equipped with all the general amenities and facilities that go\n");
+        printf("\t\t\talong with memorable stay. Set amidst beautifully landscaped gardens,\n");
+        printf("\t\t\tit proves to be a ideal dream destination for preceptive traveler.\n");
+        printf("\t\t\tthe Hotel have well furnished rooms along with rooms providing pleasant\n");
+        printf("\t\t\tviews of the city. The hotel satisfies the needs of business as well\n");
+        printf("\t\t\tas the leisure traveler. All the rooms at the Hotel are furnished\n");
+        printf("\t\t\tbeautifully. All the rooms are fitted with amenities.\n");
+        printf("\n\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t Press any key to continue...!!");
+    getch();
+    system("cls");
         printf("\n\n\n\n");
         printf("\n\t\t\t\t\t\t*************");
         printf("\n\t\t\t\t\t\t* AMENITIES *");
@@ -37,20 +39,57 @@ system("cls");
         printf("\n\t\t\t=> Free Internet Service.");
         printf("\n\t\t\t=> 24 Hours Room Service.");
         printf("\n\t\t\t=> Laundry Service.");
-        printf("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t Press any key to continue");
-getch();
-system("cls");
+        printf("\n\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t Press any key to continue...!!");
+    getch();
+
+// main menu
+
+    system("cls");
         printf("\n\n\n\n");
         printf("\n\t\t\t\t\t\t*************");
         printf("\n\t\t\t\t\t\t* MAIN MENU *");
         printf("\n\t\t\t\t\t\t*************");
         printf("\n\n\n\n");
-        printf("\n\n\n\t\t\t1.Book A Room");
-        printf("\n\t\t\t2.Customer Record");
-        printf("\n\t\t\t3.Rooms Allotted");
-        printf("\n\t\t\t4.Edit Record");
-        printf("\n\t\t\t5.Exit");
+        printf("\n\n\n\t\t\t1. Booking");
+        printf("\n\t\t\t2. Rooms Information");
+        printf("\n\t\t\t3. Rooms Allotted");
+        printf("\n\t\t\t4. Edit Record");
+        printf("\n\t\t\t0. Exit");
+
+// main menu functioning and declarations
+
+    char choice;
         printf("\n\n\t\t\tEnter Your Choice: ");
+        choice = getch();
+        system("cls");
+    switch(choice){
+            case '1':
+                    break;
+            case '2':
+
+                    system("cls");
+                    printf("\n\t\t\t\t\t\t*************");
+                    printf("\n\t\t\t\t\t\t* MAIN MENU *");
+                    printf("\n\t\t\t\t\t\t*************");
+                    printf("\n\n\n\n");
+                    printf("\t\t|Room Number\t|");
+                    printf("\tRoom Type\t|");
+                    printf("\t\tCharges per Day\t|");
+
+                    break;
+            case '3':
+//                    rooms();
+                    break;
+            case '4':
+//                    edit();
+                    break;
+            case '0': break;
+            default:
+                    printf("\n\n\t\t\tError: Please make sure Choice is Valid, Thanks!");
+                    printf("\n\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t Press any key to continue...!!");
+                    getch();
+        }
+
 
 getch();
 }
